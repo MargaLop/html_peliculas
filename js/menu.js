79a4311ed83111eefcab113392736065fa3f79b2
Marga <margalppalacios@gmail.com>
2022-07-21 17:23:47 +0200

    comienzo menu desplegable

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const iconMenu = document.querySelector('.icon-menu')
+const menu = document.querySelector('.menu-navegacion')
+
+console.log(menu)
+console.log(iconMenu)
+
+
+iconMenu.addEventListener('click', () =>{
+    alert('click')
+})
